ec91c7eac155050461f21962361bb2874b61e7f0
cel3ntano <adriano2024celentano@gmail.com>
2024-09-05 22:21:32 +0200

    created env utility, initial setupServer config

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-function setupServer() {}
+import express from 'express';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export default function setupServer() {
+  const app = express();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}

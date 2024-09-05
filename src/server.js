a294c7d60289123f26fbf0c950ad8ac40be0c8ff
cel3ntano <adriano2024celentano@gmail.com>
2024-09-05 22:36:42 +0200

    add pino

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import { env } from './utils/env.js';
+import pino from 'pino-http';
 
 const PORT = Number(env('PORT', '3000'));
 
 export default function setupServer() {
   const app = express();
+
+  app.use(
+    pino({
+      transport: { target: 'pino-pretty' },
+    }),
+  );
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });

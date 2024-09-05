93968643c7dc1932c4b9ba4cda6efe972c927412
cel3ntano <adriano2024celentano@gmail.com>
2024-09-05 14:56:52 +0200

    project configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+export default [
+  pluginJs.configs.recommended,
+  {
+    files: ["src/**/*.js"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: "error",
+      "no-unused-vars": ["error", { args: "none" }],
+      "no-undef": "error",
+    },
+  },
+];

e950c3cc198703f83af13ec72bef3f3b91654e88
cel3ntano <adriano2024celentano@gmail.com>
2024-09-05 22:30:00 +0200

    edits env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
-// dotenv.config();
+// dotenv.config({ path: '../.env' });
+dotenv.config();
 
 export function env(name, defaultValue) {
   const value = process.env[name];

b896a218c7368d7f05e631b6cf232c1000491caf
cel3ntano <adriano2024celentano@gmail.com>
2024-09-06 19:13:36 +0200

    add 404 middleware

--- src/server.js
+++ src/server.js
@@ -16,6 +16,12 @@ export default function setupServer() {
 
   app.use(cors());
 
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
